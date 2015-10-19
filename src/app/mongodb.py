4fb40d74acd97caa2edb6232386ca488ee9feb47
smlng <s@mlng.net>
2015-10-19 13:22:16 +0200

    update

--- src/app/mongodb.py
+++ src/app/mongodb.py
@@ -13,11 +13,14 @@ def get_validation_stats(dbconnstr):
     stats['num_invalid_as'] = 0
     stats['num_invalid_len'] = 0
     stats['num_not_found'] = 0
+    stats['stats'] = [['Timestamp', 'Valid', 'InvalidAS','InvalidLength', 'NotFound']]
     try:
         stats['num_valid'] = db.validity.find({'validated_route.validity.state' : 'Valid' }).count()
         stats['num_invalid_as'] = db.validity.find({'validated_route.validity.state' : 'InvalidAS' }).count()
         stats['num_invalid_len'] = db.validity.find({'validated_route.validity.state' : 'InvalidLength' }).count()
         stats['num_not_found'] = db.validity.find({'validated_route.validity.state' : 'NotFound' }).count()
+        stats_tmp = db.stats.find().map(function(s) { return [s.timestamp, s.num_valid, s.num_invalid_as, s.num_invalid_len, s.num_not_found]})
+        stats['stats'] = stats['stats'].extend(stats_tmp)
         ts_tmp = db.validity.find_one(projection={'timestamp': True, '_id': False}, sort=[('timestamp', -1)])['timestamp']
         stats['latest_ts'] = datetime.fromtimestamp(int(ts_tmp)).strftime('%Y-%m-%d %H:%M:%S')
     except Exception, e:
