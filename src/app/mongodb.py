467b0c888c38fe2f1e7d33bea89892b79ca13867
smlng <s@mlng.net>
2015-10-19 16:19:35 +0200

    fix

--- src/app/mongodb.py
+++ src/app/mongodb.py
@@ -20,14 +20,8 @@ def get_validation_stats(dbconnstr):
         stats['num_invalid_as'] = db.validity.find({'validated_route.validity.state' : 'InvalidAS' }).count()
         stats['num_invalid_len'] = db.validity.find({'validated_route.validity.state' : 'InvalidLength' }).count()
         stats['num_not_found'] = db.validity.find({'validated_route.validity.state' : 'NotFound' }).count()
-        stats_all_tmp = db.validity_stats.find({},{'_id':0})
-        for x in stats_all_tmp:
-            tts = datetime.fromtimestamp(int(x['ts']))
-            stats['stats_all'].append([datetime.fromtimestamp(int(tts)).isoformat(),x['num_valid'],x['num_invalid_len'],x['num_invalid_as'],x['num_not_found']])
-        stats_roa_tmp = db.validity_stats.find({},{'_id':0, 'num_not_found':0})
-        for y in stats_roa_tmp:
-            tts = datetime.fromtimestamp(int(y['ts']))
-            stats['stats_roa'].append([datetime.fromtimestamp(int(tts)).isoformat(),y['num_valid'],y['num_invalid_len'],y['num_invalid_as']])
+        stats['stats_all'] = db.validity_stats.find({},{'_id':0})
+        stats['stats_roa'] = db.validity_stats.find({},{'_id':0, 'num_not_found':0})
         ts_tmp = db.validity.find_one(projection={'timestamp': True, '_id': False}, sort=[('timestamp', -1)])['timestamp']
         stats['latest_ts'] = datetime.fromtimestamp(int(ts_tmp)).strftime('%Y-%m-%d %H:%M:%S')
     except Exception, e:
