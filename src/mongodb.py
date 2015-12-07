fc9433752960fca57180874f56c130df5de32216
smlng <s@mlng.net>
2015-12-07 09:22:32 +0100

    minor fix

--- src/mongodb.py
+++ src/mongodb.py
@@ -23,12 +23,13 @@ def output_stat(dbconnstr, interval):
         stats['num_invalid_len'] = 0
         stats['num_not_found'] = 0
         try:
-            stats['num_valid'] = db.validity.find({'validated_route.validity.state' : 'Valid' }).count()
-            stats['num_invalid_as'] = db.validity.find({'validated_route.validity.state' : 'InvalidAS' }).count()
-            stats['num_invalid_len'] = db.validity.find({'validated_route.validity.state' : 'InvalidLength' }).count()
-            stats['num_not_found'] = db.validity.find({'validated_route.validity.state' : 'NotFound' }).count()
-            ts_tmp = db.validity.find_one(projection={'timestamp': True, '_id': False}, sort=[('timestamp', -1)])['timestamp']
-            stats['ts'] = int(ts_tmp)
+            if "validity" in db.collection_names() and db.validity.count() > 0:
+                stats['num_valid'] = db.validity.find({'validated_route.validity.state' : 'Valid' }).count()
+                stats['num_invalid_as'] = db.validity.find({'validated_route.validity.state' : 'InvalidAS' }).count()
+                stats['num_invalid_len'] = db.validity.find({'validated_route.validity.state' : 'InvalidLength' }).count()
+                stats['num_not_found'] = db.validity.find({'validated_route.validity.state' : 'NotFound' }).count()
+                ts_tmp = db.validity.find_one(projection={'timestamp': True, '_id': False}, sort=[('timestamp', -1)])['timestamp']
+                stats['ts'] = int(ts_tmp)
         except Exception, e:
             logging.exception ("QUERY failed with: " + e.message)
         else:
