7c3e7a33e29463be0b80d6132f0b9198c4d7bf76
smlng <s@mlng.net>
2016-10-12 12:14:22 +0200

    fix loop

--- src/bgpstreamUpdateParser.py
+++ src/bgpstreamUpdateParser.py
@@ -76,7 +76,7 @@ def recv_bgpstream_rib(begin, until, collector, output_queue):
         while (elem):
             if ((next_hop != elem.fields['next-hop']) or (ts != elem.time) or
                 (src_addr != elem.peer_address) or (src_asn != elem.peer_asn) or
-                (aspath != elem.fields['as-path']) or not bgp_message):
+                (aspath != elem.fields['as-path'])):
                 if bgp_message: # output previous data
                     output_queue.put(bgp_message)
                 ts = elem.time
@@ -90,11 +90,14 @@ def recv_bgpstream_rib(begin, until, collector, output_queue):
                 src_peer['port'] = 0
                 src_peer['asn'] = src_asn
                 bgp_message.set_source(src_peer)
-                aspath = elem.fields['as-path'].split()
-                for a in aspath:
+                aspath = elem.fields['as-path']
+                for a in aspath.split():
                     if not '{' in a: # ignore AS-SETs
                         bgp_message.add_as_to_path(a)
             bgp_message.add_announce(elem.fields['prefix'])
+            elem = rec.get_next_elem()
+        # end while (elem)
+    # end while (stream...)
 
 
 def recv_bgpstream_updates(begin, until, collector, output_queue):
